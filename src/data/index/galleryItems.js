6ed6271bc714a3f59347194a48f94c46842b6490
car06 <221220@ids.upchiapas.edu.mx>
2025-07-19 22:22:25 -0600

    path de imagenes actualizado

--- src/data/index/galleryItems.js
+++ src/data/index/galleryItems.js
@@ -1,34 +1,10 @@
 export const galleryItems = [
-    { 
-      image: "../../../public/img/index/1.jpeg", 
-      text: "Panal Natural" 
-    },
-    { 
-      image: "../../../public/img/index/2.jpeg", 
-      text: "Apicultor Experto" 
-    },
-    { 
-      image: "../../../public/img/index/3.jpeg", 
-      text: "Colmena Saludable" 
-    },
-    { 
-      image: "../../../public/img/index/4.jpeg", 
-      text: "Miel Pura" 
-    },
-    { 
-      image: "../../../public/img/index/5.jpeg", 
-      text: "Cera Artesanal" 
-    },
-    { 
-      image: "../../../public/img/index/6.jpeg", 
-      text: "Polinización Natural" 
-    },
-    { 
-      image: "../../../public/img/index/7.jpeg", 
-      text: "Colmenar Tradicional" 
-    },
-    { 
-      image: "../../../public/img/index/8.jpeg", 
-      text: "Cosecha Sostenible" 
-    }
+  { image: "/img/index/1.jpeg", text: "Panal Natural" },
+  { image: "/img/index/2.jpeg", text: "Apicultor Experto" },
+  { image: "/img/index/3.jpeg", text: "Colmena Saludable" },
+  { image: "/img/index/4.jpeg", text: "Miel Pura" },
+  { image: "/img/index/5.jpeg", text: "Cera Artesanal" },
+  { image: "/img/index/6.jpeg", text: "Polinización Natural" },
+  { image: "/img/index/7.jpeg", text: "Colmenar Tradicional" },
+  { image: "/img/index/8.jpeg", text: "Cosecha Sostenible" }
 ];
