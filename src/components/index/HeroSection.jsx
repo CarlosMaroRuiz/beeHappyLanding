1cb31f168a4640049e810527e1e20739d366d40a
car06 <221220@ids.upchiapas.edu.mx>
2025-07-19 22:19:16 -0600

    imagenes reales index

--- src/components/index/HeroSection.jsx
+++ src/components/index/HeroSection.jsx
@@ -1,21 +1,9 @@
 import CircularGallery from './CircularGallery'
+import { galleryItems } from '../../data/index/galleryItems'
 
 const HeroSection = () => {
-  // Datos para el carrusel con imágenes originales
-  const galleryItems = [
-    { 
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRtmI-1KleiJxP6bXS-6xZAAopMY6UMPLS1A&s", 
-      text: "Panal Natural" 
-    },
-    { 
-      image: "https://www.abejareyna.mx/cdn/shop/articles/importancia-abejas-entrevista-frida-rivera-revista-glamour.webp?v=1719966545", 
-      text: "Apicultor Experto" 
-    },
-    { 
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUOPATfV5p6BMHOKuMAtvQh08TOvsTjXFXNw&s", 
-      text: "Colmena Saludable" 
-    }
-  ];
+
+  
 
   return (
     <section className="w-full px-12 mt-8">
---
+++ src/data/index/galleryItems.js
@@ -0,0 +1,34 @@
+export const galleryItems = [
+    { 
+      image: "../../../public/img/index/1.jpeg", 
+      text: "Panal Natural" 
+    },
+    { 
+      image: "../../../public/img/index/2.jpeg", 
+      text: "Apicultor Experto" 
+    },
+    { 
+      image: "../../../public/img/index/3.jpeg", 
+      text: "Colmena Saludable" 
+    },
+    { 
+      image: "../../../public/img/index/4.jpeg", 
+      text: "Miel Pura" 
+    },
+    { 
+      image: "../../../public/img/index/5.jpeg", 
+      text: "Cera Artesanal" 
+    },
+    { 
+      image: "../../../public/img/index/6.jpeg", 
+      text: "Polinización Natural" 
+    },
+    { 
+      image: "../../../public/img/index/7.jpeg", 
+      text: "Colmenar Tradicional" 
+    },
+    { 
+      image: "../../../public/img/index/8.jpeg", 
+      text: "Cosecha Sostenible" 
+    }
+];
